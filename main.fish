7b260c10a3add32d85fe9c946367e8532f87d074
FliiFe <theophile.cailliau@gmail.com>
2017-12-06 15:51:58 +0100

    Fix typo

--- main.fish
+++ main.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/env fish
 
-set path (curl 'http://www.lyceecolbert-tg.org/menu-vivre/restauration-scolaire.html' ^/dev/null | grep -Poi '<p.+?img.+?/>' | grep -Po '".+?"' | tail -n 1 | string trim -c '"''"')
+set path (curl 'http://www.lyceecolbert-tg.org/menu-vivre/restauration-scolaire.html' ^/dev/null | grep -Poi '<p.+?img.+?/>' | grep -Po '".+?"' | tail -n 1 | string trim -c '"')
 set url "http://www.lyceecolbert-tg.org/$path"
 
 wget $url -O menu.jpg
